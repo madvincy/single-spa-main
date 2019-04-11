9d75b5b6b6fd5a990ca6239721d122a8ce535222
corneliouzbett <kbett68@gmail.com>
2019-04-11 14:54:09 +0300

    App skeleton added

--- patientdashboard/src/app/app.module.ts
+++ patientdashboard/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PatientSearchService } from './app.service';
-import { PatientResourceService } from './services/patient-resource.service';
-import { HttpClientModule } from '@angular/common/http';
 import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
@@ -14,14 +11,9 @@ import { APP_BASE_HREF } from '@angular/common';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [
-    {provide: APP_BASE_HREF, useValue: '/search'},
-    PatientSearchService,
-    PatientResourceService
-  ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/patientdashboard'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- patientdashboard/src/main.single-spa.ts
+++ patientdashboard/src/main.single-spa.ts
@@ -3,7 +3,7 @@ import { enableProdMode, NgZone } from '@angular/core';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Router } from '@angular/router';
-import { ɵAnimationEngine as AnimationEngine } from '@angular/animations/browser';
+import { ɵAnimationEngine as AnimationEngine } from '@angular/animations/browser'; 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import singleSpaAngular from 'single-spa-angular';
@@ -16,5 +16,6 @@ export default singleSpaAngular({
   bootstrapFunction: () => platformBrowserDynamic().bootstrapModule(AppModule),
   template: '<app-root />',
   Router,
-  NgZone: NgZone
+  NgZone: NgZone,
+  AnimationEngine: AnimationEngine, 
 });
